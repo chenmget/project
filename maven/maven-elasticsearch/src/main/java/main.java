54a3e895277aee2c9f64d1888d32c8c1a8481513
chenmget <502298798@qq.com>
2019-03-15 15:52:26 +0800

    es

--- maven/maven-elasticsearch/src/main/java/main.java
+++ maven/maven-elasticsearch/src/main/java/main.java
@@ -12,34 +12,12 @@ import org.elasticsearch.transport.client.PreBuiltTransportClient;
 public class main {
 	
 	 public final static String HOST = "127.0.0.1";
-	    // http����Ķ˿���9200���ͻ�����9300
 	    public final static int PORT = 9300; 
 	public static void main(String[] args) throws IOException {
-//		 String hostName = "127.0.0.1";
-//		// on startup
-//		 Settings settings = Settings.builder().put("cluster.name","elasticsearch").build();
-//	    //TransportAddress transportAddress = new TransportAddress(InetAddress.getByName(hostName), 9300);
-//	    TransportClient client = new PreBuiltTransportClient(settings).
-//	    		addTransportAddress(new TransportAddress(InetAddress.getByName("localhost"),9200));
-//	    client.prepareIndex("1", "1").setSource("1", XContentType.JSON);
-//		 // on shutdown
-//		 client.close();
+
 		Settings settings = Settings.builder().put("cluster.name", "elasticsearch").build();
-        // ����client
         TransportClient client = new PreBuiltTransportClient(settings)
                 .addTransportAddresses(new TransportAddress(InetAddress.getByName(HOST), PORT));
-        
-//        String json = "{" +
-//                "\"user\":\"kimchy\"," +
-//                "\"postDate\":\"2013-01-30\"," +
-//                "\"message\":\"trying out Elasticsearch\"" +
-//            "}";
-//
-//        IndexResponse response = client.prepareIndex("twitter", "_doc")
-//                .setSource(json, XContentType.JSON)
-//                .get();
-//        System.out.println(response.getId());  //LGdNnWgBSP69BoX8m-_g
-        
         GetResponse response = client.prepareGet("twitter", "_doc", "LGdNnWgBSP69BoX8m-_g").get();
         System.out.println(response);
         
